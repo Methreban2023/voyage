d9ec40e4c08e4b5d089ceb661dbd892b9ab190f4
methreban2023 <alrefai.dalia@gmail.com>
2023-07-27 17:00:10 +0300

    update profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Button } from "react-native-paper";
 import UserContext from "../context/UserContext";
 import { removeToken } from "../apis/auth/storage";
@@ -7,12 +7,19 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { colors } from "../utils/colors/colors";
 import { MaterialIcons } from "@expo/vector-icons";
+import ROUTES from "../navigation/routes";
+
 const Profile = ({ navigation }) => {
   const { setUser } = useContext(UserContext);
+  // const [userInfo, setUserInfo] = useState({});
+  // const editProfileHandler = () => {
+  //   navigation.navigate(ROUTES.APPROUTES.EDITPROFILE, { userInfo, setUserInfo } });
+  // };
   const pressHandler = () => {
     removeToken();
     setUser(false);
   };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
       <View>
@@ -29,6 +36,17 @@ const Profile = ({ navigation }) => {
             color={colors.black}
           />
         </TouchableOpacity>
+
+        <View
+          style={{
+            backgroundColor: colors.baby_blue,
+            marginTop: 50,
+          }}
+        >
+          {/* <TouchableOpacity onPress={editProfileHandler}>
+            <Text>Edit my Profile </Text>
+          </TouchableOpacity> */}
+        </View>
         <Button
           title="SignOut"
           onPress={() => {
