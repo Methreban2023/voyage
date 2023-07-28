0012d6b13f023c38774a147759b322bed2b0e27f
Najat Mohammed <najatmohammed@Najats-MBP.net>
2023-07-28 21:13:25 +0300

    friday night

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.8.127:8000";
-// const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "http://192.168.8.127:8000";
+const BASE_URL = "http://localhost:8000";
 
 const instance = axios.create({
   baseURL: BASE_URL + "/api",
